06c69fe7366335005b31c855b55c1a4495c8124c
Sunny Sharma <syn8433898723@gmail.com>
2026-04-23 13:39:01 +0530

    chore: initialize Spring Boot project with all dependencies

---
+++ jobboard/src/main/java/com/sunny/jobboard/JobboardApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobboardApplication.class, args);
+	}
+
+}
---
+++ jobboard/src/test/java/com/sunny/jobboard/JobboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
